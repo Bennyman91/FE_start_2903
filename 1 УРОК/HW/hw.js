edf5babb0aa38960d505520c5533a9f0397125bc
Bennyman91 <ben4ikman@gmail.com>
2024-05-18 22:13:16 +0300

    study

---
+++ 1 УРОК/HW/hw.js
@@ -0,0 +1,8 @@
+let userAgeValue = 33;
+let userNameNickname = 'Vladislav';
+console.log(`userAge: ${userAgeValue} userName: ${userNameNickname}`);
+
+userAgeValue = 25;
+userNameNickname = 'Jessy';
+
+console.log(`userAge: ${userAgeValue} userName: ${userNameNickname}`);
